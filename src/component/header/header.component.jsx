b1f06a9ab7faebef704bb8abf00cd6a4ba54e61c
manolph <manol@webpoint.io>
2019-08-23 12:13:17 -0500

    added redux and implemented userReducer and userActions to header and app components

--- src/component/header/header.component.jsx
+++ src/component/header/header.component.jsx
@@ -1,24 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import './header.styles.scss';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
-const Header = ({ currentUser }) => (
-    <div className="header">
-       <Link to="/" className="logo-container">
-            <Logo className="logo"/>
-       </Link>
 
-       <div className="options">
-        <Link className="option" to="/shop">SHOP</Link>
-        <Link className="option" to="/contact">CONTACT</Link>
-        {
-            currentUser ? 
-            <div className="option" onClick={ () => auth.signOut() }> SIGN OUT</div> : 
-            <Link className="option" to="/signin">SIGN IN</Link>
-        }
-       </div>
-    </div>
-);
 
-export default Header;
+
+const Header = ({ currentUser }) => {
+    console.log(currentUser)
+    return (
+        <div className="header">
+           <Link to="/" className="logo-container">
+                <Logo className="logo"/>
+           </Link>
+    
+           <div className="options">
+            <Link className="option" to="/shop">SHOP</Link>
+            <Link className="option" to="/contact">CONTACT</Link>
+            {
+                currentUser ? 
+                <div className="option" onClick={ () => auth.signOut() }> SIGN OUT</div> : 
+                <Link className="option" to="/signin">SIGN IN</Link>
+            }
+           </div>
+        </div>
+    );
+}
+
+const mapStateToProps = (state) => {
+    return {
+        currentUser: state.user.currentUser
+    }
+}
+
+export default connect(mapStateToProps)(Header);
--- src/component/sign-in/sign-in.component.jsx
+++ src/component/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 
 class SignIn extends React.Component {
@@ -17,10 +17,16 @@ class SignIn extends React.Component {
 
     handleSubmit = (event) => {
         event.preventDefault();
-        this.setState({ email: '', password:'' });
+        const { email, password } = this.state;
+        try {
+            auth.signInWithEmailAndPassword(email, password)
+            this.setState({ email: '', password: '' })
+        } catch (error) {
+            console.error(error);
+        }
     }
 
-    handleChange = (event) => {
+    handleChange = async (event) => {
         const { name, value } = event.target; 
         this.setState({ [name]: value })
     }
