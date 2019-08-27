dcf8a69d918e9ccad29bb25c542dbeb2cf0b0647
manolph <manol@webpoint.io>
2019-08-27 11:08:40 -0500

    added addItem and removeItem functionality to checkout item component

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,3 +1,4 @@
+
 export const addItemToCart = (cartItems, cartItemToAdd) => {
     const existingCartItem  = cartItems.find(
         cartItem => cartItem.id === cartItemToAdd.id 
@@ -14,4 +15,22 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
         return [
             ...cartItems, {...cartItemToAdd, quantity: 1}
         ]
+}
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    const existingCartItem = cartItems.find(cartItem => 
+         cartItem.id === cartItemToRemove.id
+    )
+
+    if(existingCartItem.quantity === 1) {
+       return cartItems.filter(cartItem => 
+            cartItem.id !== cartItemToRemove.id
+        )
+    }
+
+    return cartItems.map(cartItem => {
+      return cartItem.id === cartItemToRemove.id ?
+       {...cartItem, quantity: cartItem.quantity - 1} :
+       cartItem
+    })
 }
